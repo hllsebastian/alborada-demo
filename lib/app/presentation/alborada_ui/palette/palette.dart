1ff3e337a2a3118c1a24cfd07c4673261028995f
hllsebastian <84684391+hllsebastian@users.noreply.github.com>
2024-11-09 23:00:31 -0500

    Color palette

---
+++ lib/app/presentation/alborada_ui/palette/palette.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class Palette {
+  const Palette._();
+  // Black
+  static const Color black = Color(0xFF222222);
+  // Grey
+  static const Color darkGrey = Color(0xFF383838);
+  static const Color coldGrey = Color(0xFF62667D);
+  static const Color midGrey = Color(0xFFC0C1C9);
+  static const Color lightGrey = Color(0xFFE5E5E6);
+  // yellows
+  static const Color yellow100 = Color(0xFFFEC574);
+  static const Color yellow80 = Color(0xFFFCD586);
+  static const Color yellow20 = Color(0xFFFEF3E1);
+  static const Color yellow5 = Color(0xFFFFFBF5);
+  // White
+  static const Color white = Color(0xFFFFFFFF);
+}
