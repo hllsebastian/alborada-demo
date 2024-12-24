d7863dba7fab799518f4131f1625265eab6c43c2
hllsebastian <84684391+hllsebastian@users.noreply.github.com>
2024-12-24 17:35:19 -0500

    Adding packages and connection with Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:alborada_demo/app/alborada_app.dart';
+import 'package:alborada_demo/app/di/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: 'https://sjxtxbnwxhhuqurkoecn.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeHR4Ym53eGhodXF1cmtvZWNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3MzI0NjAsImV4cCI6MjA1MDMwODQ2MH0.JRA3yXgwWkfpVhYr4ivinB3WkGZZam8EXpDVoGhug0A',
+  );
+  setupServiceLocator();
   runApp(const AlboradaApp());
 }
