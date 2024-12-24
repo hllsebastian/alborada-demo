48814f88f242791997c3dfcef408f17aa2ff0ef5
hllsebastian <84684391+hllsebastian@users.noreply.github.com>
2024-12-24 18:43:50 -0500

    Creating Logger

--- lib/app/di/service_locator.dart
+++ lib/app/di/service_locator.dart
@@ -1,7 +1,8 @@
-import 'package:alborada_demo/app/data/data_sources/auth_remote_data_source.dart';
+import 'package:alborada_demo/app/core/core.dart';
+import 'package:alborada_demo/app/data/data_sources/remote/auth_remote_data_source.dart';
 import 'package:alborada_demo/app/data/repositories/auth_repository_impl.dart';
 import 'package:alborada_demo/app/domain/repositories/auth_repository.dart';
-import 'package:alborada_demo/app/domain/use_cases/signin_use_case.dart';
+import 'package:alborada_demo/app/domain/use_cases/create_account_use_case.dart';
 import 'package:alborada_demo/app/presentation/views/create_account.dart/cubit/create_account_cubit.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -9,12 +10,15 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 final getIt = GetIt.instance;
 
 void setupServiceLocator() {
-  // Supabase client
+// Supabase client
   getIt.registerLazySingleton<SupabaseClient>(
     () => Supabase.instance.client,
   );
 
-  // Datasource
+// Logger
+  getIt.registerLazySingleton<Logger>(() => Logger());
+
+// Datasource
   getIt.registerLazySingleton<AuthRemoteDataSource>(
     () => AuthRemoteDataSourceImpl(getIt<SupabaseClient>()),
   );
@@ -24,11 +28,11 @@ void setupServiceLocator() {
       () => AuthRepositoryImpl(getIt<AuthRemoteDataSource>()));
 
 // Use Case
-  getIt.registerLazySingleton<SigninUseCase>(
-      () => SigninUseCase(getIt<AuthRepository>()));
+  getIt.registerLazySingleton<CreateAccountUseCase>(
+      () => CreateAccountUseCase(getIt<AuthRepository>()));
 
 // Cubits
   // getIt.registerFactory<SigninCubit>(() => SigninCubit(getIt<SigninUseCase>()));
   getIt.registerFactory<CreateAccountCubit>(
-      () => CreateAccountCubit(getIt<SigninUseCase>()));
+      () => CreateAccountCubit(getIt<CreateAccountUseCase>()));
 }
