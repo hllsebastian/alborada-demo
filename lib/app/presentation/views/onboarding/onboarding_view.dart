c12f9c0530a4fabc812cc38532317e3808527341
hllsebastian <84684391+hllsebastian@users.noreply.github.com>
2025-02-15 04:14:48 -0500

    Changing Curve animation in pageController

--- lib/app/presentation/views/onboarding/onboarding_view.dart
+++ lib/app/presentation/views/onboarding/onboarding_view.dart
@@ -1,14 +1,13 @@
 import 'package:alborada_demo/app/presentation/alborada_ui/alborada_ui.dart';
 import 'package:alborada_demo/app/presentation/routes/routes.dart';
 import 'package:alborada_demo/app/presentation/views/cubit/user_cubit/user_cubit.dart';
-import 'package:alborada_demo/app/presentation/views/onboarding/cubit/onboarding_cubit.dart';
-import 'package:alborada_demo/app/presentation/views/onboarding/pages/choise_initiative_page.dart';
-import 'package:alborada_demo/app/presentation/views/onboarding/pages/reap_reward_page.dart';
-import 'package:alborada_demo/app/presentation/views/onboarding/pages/take_action_page.dart';
+import 'package:alborada_demo/app/presentation/views/onboarding/pages/pages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'cubit/onboarding_cubit.dart';
 
 class OnboardingViewArguments {
   final User user;
@@ -79,7 +78,18 @@ class _OnboardingViewState extends State<OnboardingView> {
                     isDarkButton: true,
                     text: _currentPage == 2 ? 'C’est compris!' : 'Suivant',
                     onPressed: () {
-                      if (_currentPage == 2) {
+                      final nextPage = _currentPage + 1;
+                      if (nextPage < onboardingPages.length) {
+                        setState(() {
+                          _currentPage = nextPage;
+                        });
+
+                        _pageController.animateToPage(
+                          nextPage,
+                          curve: Curves.fastOutSlowIn,
+                          duration: Durations.extralong1,
+                        );
+                      } else {
                         final user = context.read<UserCubit>().state;
                         if (user != null) {
                           context.read<OnboardingCubit>().createUser(user);
@@ -90,18 +100,6 @@ class _OnboardingViewState extends State<OnboardingView> {
                           (route) => false,
                         );
                       }
-                      if (_currentPage == onboardingPages.length - 1) {
-                        _pageController.animateToPage(
-                          0,
-                          duration: Durations.extralong3,
-                          curve: Curves.easeInOut,
-                        );
-                        return;
-                      }
-                      _pageController.nextPage(
-                        duration: Durations.extralong3,
-                        curve: Cubic(0.68, -0.55, 0.27, 1.55),
-                      );
                     },
                   ),
                 ],
