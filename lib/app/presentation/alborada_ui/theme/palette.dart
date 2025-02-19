67d10a5932db8d97730b11781ec448da4ab5d289
hllsebastian <84684391+hllsebastian@users.noreply.github.com>
2025-02-18 21:54:17 -0500

    Button color

--- lib/app/presentation/alborada_ui/theme/palette.dart
+++ lib/app/presentation/alborada_ui/theme/palette.dart
@@ -5,13 +5,15 @@ class Palette {
   // Black
   static const Color black = Color(0xFF222222);
   // Grey
+  static const Color grey1 = Color(0xFFE5E5E6);
+  static const Color grey2 = Color(0xFFC0C1C9);
   static const Color grey3 = Color(0xFF62667D);
   static const Color darkGrey = Color(0xFF383838);
   static const Color coldGrey = Color(0xFF62667D);
   static const Color midGrey = Color(0xFFC0C1C9);
   static const Color lightGrey = Color(0xFFE5E5E6);
   // yellows
-  static const Color yellow100 = Color(0xFFFEC574);
+  static const Color yellow100 = Color(0xFFFEC574); // primaryYeloow
   static const Color yellow80 = Color(0xFFFCD586);
   static const Color yellow20 = Color(0xFFFEF3E1);
   static const Color yellow5 = Color(0xFFFFFBF5);
