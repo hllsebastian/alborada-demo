ae307958f4488008c4e8bd657d66b0376a4d10f0
hllsebastian <84684391+hllsebastian@users.noreply.github.com>
2025-03-29 00:09:30 -0500

    Widgets for info buttons

--- lib/app/presentation/alborada_ui/widgets/buttons/buttons.dart
+++ lib/app/presentation/alborada_ui/widgets/buttons/buttons.dart
@@ -1,5 +1,6 @@
 export 'arrow_back_button.dart';
 export 'button_icon_widget.dart';
 export 'home_back_button.dart';
+export 'info_button.dart';
 export 'initiatives_buttons.dart';
 export 'simple_button.dart';
